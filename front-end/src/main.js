b0d4ac9a7570abe7c46d605b22776ea739d08776
hym66 <2212707236@qq.com>
2022-10-15 15:30:39 +0800

    main.js引入element-ui
    main.js引入element-ui

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//引入element-ui
+import 'element-ui/lib/theme-chalk/index.css'
+
+
+// 按需引入Element-ui
+import {
+  Button
+} from 'element-ui'
+
+//注册使用element-ui
+Vue.use(Button)
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
