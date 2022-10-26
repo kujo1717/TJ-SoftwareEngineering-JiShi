3a9e2c3d757bc14e711cea88952df5c8fe97e564
hym66 <2212707236@qq.com>
2022-10-26 23:02:59 +0800

    创建后端项目

---
+++ back-end/src/main/java/com/example/backend/entity/User.java
@@ -0,0 +1,11 @@
+package com.example.backend.entity;
+
+public class User {
+    private String name;
+    private String pwd;
+
+    public User(String name, String pwd) {
+        this.name = name;
+        this.pwd = pwd;
+    }
+}
