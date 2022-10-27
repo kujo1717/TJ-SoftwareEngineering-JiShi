5c101bce6c3a6d86c1bc018e0ebc76ae660a2812
hym66 <2212707236@qq.com>
2022-10-27 23:35:58 +0800

    更改笔误

--- back-end/src/main/java/com/example/backend/mapper/UserMapper.java
+++ back-end/src/main/java/com/example/backend/mapper/UserMapper.java
@@ -3,11 +3,13 @@ package com.example.backend.mapper;
 import com.example.backend.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
 public interface UserMapper {
-    @Select("selecgt * from user")
+    @Select("select * from user")
     List<User> findAll();
 }
