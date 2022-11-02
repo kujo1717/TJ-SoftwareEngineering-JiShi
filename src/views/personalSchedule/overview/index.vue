d30a5cb095efdccf1cdfae80d17b86e1836e0c6d
kujo1717 <82948895+kujo1717@users.noreply.github.com>
2022-11-02 19:46:54 +0800

    改用el-admin框架

---
+++ src/views/personalSchedule/overview/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-col
+      :xs="{ span: 24 }"
+      :sm="{ span: 24 }"
+      :md="{ span: 24 }"
+      :lg="{ span: 12 }"
+      :xl="{ span: 12 }"
+      style="padding-right: 8px; margin-bottom: 30px"
+    >
+<intro-card>
+</intro-card>
+    </el-col>
+  </div>
+</template>
+<script>
+import IntroCard from "./components/IntroCard.vue"
+export default {
+  name: "OverView",
+  components:{IntroCard},
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+</style>
+
---
+++ src/views/teams/team2/proj2-1/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div style="padding:30px;">
+    <el-alert :closable="false" title="proj 2-2" type="success">
+      <router-view />
+    </el-alert>
+  </div>
+</template>
