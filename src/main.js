d2dbf0c186c02b344cf0c1db495bf1428ea91e67
hym66 <2212707236@qq.com>
2022-11-02 10:06:05 +0800

    添加echarts依赖
    1.添加echarts依赖
    2.改回原来的App.vue

--- src/main.js
+++ src/main.js
@@ -18,6 +18,9 @@ import '@/permission' // permission control
 
 import './assets/styles/icon/iconfont.css'
 
+import echarts from 'echarts'
+Vue.prototype.$echarts = echarts
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
