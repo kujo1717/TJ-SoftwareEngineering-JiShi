6c6220e1a97952497b145324d945d898c3f961d0
hym66 <2212707236@qq.com>
2022-12-02 22:50:27 +0800

    增加工具类的方法

--- src/main/java/com/example/backend/Tools/DateTimeUtil.java
+++ src/main/java/com/example/backend/Tools/DateTimeUtil.java
@@ -16,4 +16,26 @@ public class DateTimeUtil {
         LocalDateTime parse = LocalDateTime.parse(time, formatter);
         return LocalDateTime.from(parse).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
+
+    /**
+     * @description: 获取某一个月的天数
+     * @author: hym
+     * @date: 2022/11/25 10:35
+     * @param:
+     * @param: year
+     * @param: month
+     * @return:
+     * @return: int
+     **/
+    public static int getDayNumOfMonth(int year, int month) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(Integer.toString(year) + "-" + Integer.toString(month) + "-01");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+    }
+
+    public static Timestamp getCurrentTimestamp(){
+        return new Timestamp(new Date().getTime());
+    }
 }
