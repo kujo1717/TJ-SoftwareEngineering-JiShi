abfc8e7d023bb8dcb91d92110314bec068791790
lishangru <947112895@qq.com>
2022-12-06 08:21:34 +0800

    新增vote

--- back-end/src/main/java/com/example/backend/service/impl/ActivityParticipateServiceImpl.java
+++ back-end/src/main/java/com/example/backend/service/impl/ActivityParticipateServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.backend.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.backend.dto.ActivityUserRole;
+import com.example.backend.entity.Activity;
 import com.example.backend.entity.ActivityApply;
 import com.example.backend.entity.ActivityParticipate;
 import com.example.backend.mapper.ActivityParticipateMapper;
@@ -64,5 +66,20 @@ public class ActivityParticipateServiceImpl implements ActivityParticipateServic
         List<Map<String,Object>> list=new ArrayList<>();
         list=activityParticipateMapper.SelectActApplicantList(activity_id);
         return list;
+    }
+    @Override
+    public ActivityUserRole isMember(Long user_id, Long activity_id) {
+        ActivityUserRole role=new ActivityUserRole();
+        role.setActivity_id(activity_id);
+        role.setUser_id(user_id);
+
+        QueryWrapper<ActivityParticipate> queryWrapper=new QueryWrapper<>();
+        queryWrapper
+                .eq("user_id",user_id)
+                .eq("activity_id",activity_id);
+        Integer count=activityParticipateMapper.selectCount(queryWrapper);
+        role.setIs_member(count==1?true:false);
+        return role;
+
     }
 }
