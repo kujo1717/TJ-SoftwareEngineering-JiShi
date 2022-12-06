50c3d2458b3b7b1ad066f216507e6c24f4df6d8e
lishangru <947112895@qq.com>
2022-12-06 08:44:32 +0800

    新建vote

--- back-end/src/main/java/com/example/backend/dto/ActivityUserRole.java
+++ back-end/src/main/java/com/example/backend/dto/ActivityUserRole.java
@@ -6,7 +6,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
-
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.Data;
 /**
  * @Author: 杨严
  * @Date: 2022/11/26/12:53 PM
@@ -21,5 +26,7 @@ public class ActivityUserRole {
     private Long user_id;
     private Boolean is_creator;
     private Boolean is_applicant;
+    private Boolean is_member;
 
 }
+
